51cc9ed224d94e6c400403425ac25308cd345a1e
vproskurin <vladimir.proskurin@gmail.com>
2020-04-15 10:00:11 -0700

    Basic alculator ops done

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # vue-calc
 
+A basic calculator done with Vue.js. Right now there are some bugs.
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,23 +3,23 @@
     <div class="display"> {{current || '0' }} </div>
     <div @click="clear" class="button">C</div>
     <div @click="sign" class="button">+/-</div>
-    <div class="button">%</div>
-    <div class="button operator">/</div>
-    <div class="button">7</div>
-    <div class="button">8</div>
-    <div class="button">9</div>
-    <div class="button operator">x</div>
-    <div class="button">4</div>
-    <div class="button">5</div>
-    <div class="button">6</div>
-    <div class="button operator">-</div>
-    <div class="button">1</div>
-    <div class="button">2</div>
-    <div class="button">3</div>
-    <div class="button operator">+</div>
-    <div class="button zero">0</div>
-    <div class="button">.</div>
-    <div class="button operator">=</div>
+    <div @click="percent" class="button">%</div>
+    <div @click="divide" class="button operator">/</div>
+    <div @click="append('7')" class="button">7</div>
+    <div @click="append('8')" class="button">8</div>
+    <div @click="append('9')" class="button">9</div>
+    <div @click="multiply" class="button operator">x</div>
+    <div @click="append('4')" class="button">4</div>
+    <div @click="append('5')" class="button">5</div>
+    <div @click="append('6')" class="button">6</div>
+    <div @click="minus" class="button operator">-</div>
+    <div @click="append('1')" class="button">1</div>
+    <div @click="append('2')" class="button">2</div>
+    <div @click="append('3')" class="button">3</div>
+    <div @click="plus" class="button operator">+</div>
+    <div @click="append('0')" class="button zero">0</div>
+    <div @click="dot" class="button">.</div>
+    <div @click="equals" class="button operator">=</div>
     
   </div>
 </template>
@@ -28,8 +28,10 @@
 export default {
   data() {
     return{
-      current:'123',
-
+      previous: null,
+      current: '',
+      operator: null,
+      operatorClicked: false
     }
   },
   methods: {
@@ -39,6 +41,48 @@ export default {
     sign() {
       this.current = this.current.charAt(0) === "-" ? 
         this.current.slice(1) : `-${this.current}`
+    },
+    percent() {
+      this.current = `${parseFloat(this.current)/100}`
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current=''
+        this.operatorClicked = false
+      }
+      this.current = `${this.current}${number}`
+    },
+    dot() {
+      if (this.current.lastIndexOf('.') === -1) {
+        this.append('.')
+      }
+    },
+    setPrevious() {
+      this.previous = this.current
+      this.operatorClicked = true
+    },
+    divide() {
+      this.operator = (a,b) => a / b ;
+      this.setPrevious()
+    },
+    multiply() {
+      this.operator = (a,b) => a * b ;
+      this.setPrevious()
+    },
+    minus() {
+      this.operator = (a,b) => a - b ;
+      this.setPrevious()
+    },
+    plus() {
+      this.operator = (a,b) => a + b ;
+      this.setPrevious()
+    },
+    equals() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+        )}`
+        this.previous = null
     }
   }
 
